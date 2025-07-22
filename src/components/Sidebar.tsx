5859c0bc984c94d389e21f8b68090b985e4baed3
Mahmoud123-Makhaly <mahmoud.abdelfatah@5d-agency.com>
2025-07-22 13:59:51 +0300

    -fix issue in sidebar filtration

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,47 +2,42 @@ import { useState } from "react";
 import { useShoppingCart } from "./Store";
 
 const Sidebar = () => {
-  const { setProducts, allProducts } = useShoppingCart();
+  const { allProducts, setProducts } = useShoppingCart();
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
-  const categories = [...new Set(allProducts.map((p) => p.category))];
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const categories = [
+    ...new Set(allProducts.map((product) => product.category)),
+  ];
+  const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const category = e.target.value;
-
     const isChecked = e.target.checked;
-    const updatedSelectedCategories = isChecked
+    const updatedCategories = isChecked
       ? [...selectedCategories, category]
-      : selectedCategories.filter((c) => c !== category);
+      : selectedCategories.filter((categories) => categories !== category);
 
-    setSelectedCategories(updatedSelectedCategories);
-
-    const filtered =
-      updatedSelectedCategories.length === 0
+    setSelectedCategories(updatedCategories);
+    const filteredProducts =
+      updatedCategories.length === 0
         ? allProducts
-        : allProducts.filter(
-            (
-              p //if condition is true return the product
-            ) => updatedSelectedCategories.includes(p.category)
+        : allProducts.filter((product) =>
+            updatedCategories.includes(product.category)
           );
-
-    setProducts(filtered);
+    setProducts(filteredProducts);
   };
-
   return (
     <div className="border p-3 rounded flex flex-col items-end">
       <h2 className="font-semibold mb-3 text-[1.2rem]">Filter By Category</h2>
       <form>
         <ul className="flex-col">
           {categories.map((category) => (
-            <li key={category} className="flex justify-end">
+            <li key={category} className="flex justify-end mb-3">
               <div className="flex items-center gap-3">
                 <label>{category}</label>
                 <input
                   type="checkbox"
                   value={category}
                   checked={selectedCategories.includes(category)}
-                  onChange={handleChange}
+                  onChange={handelChange}
                 />
               </div>
             </li>
